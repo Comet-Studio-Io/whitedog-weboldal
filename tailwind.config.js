fdbf766bf0ce62f2215a5002eeaf793de2b273fa
Armand Gonda <armand.gonda@5martstudio.com>
2022-09-26 17:42:27 +0200

    feat: add font to webpge

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        primary: ["Oracle", "regular"],
+      },
+    },
   },
   plugins: [],
 };
