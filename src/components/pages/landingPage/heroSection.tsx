37cb1e425e58ae6dd40ddca02fa02550a7732e77
Armand Gonda <armand.gonda@5martstudio.com>
2022-09-27 15:24:22 +0200

    chore: move herosection location

--- src/components/pages/landingPage/heroSection.tsx
+++ src/components/pages/landingPage/heroSection.tsx
@@ -3,20 +3,20 @@ import { memo, FC } from "react";
 
 const HeroSectionComponent: FC = (): JSX.Element => {
   return (
-    <section className="w-full h-1/2 flex flex-col">
+    <section className="w-full h-auto flex flex-col">
       <Image
         alt="hero-image"
-        className="absolute z-0"
+        className="absolute"
         height={720}
         layout="responsive"
         src="/images/hero.png"
         width={1440}
       />
-      <div className="relative -top-44 ml-7">
-        <span className="text-white flex text-[24px] text-opacity-50 font-light uppercase">
+      <div className="relative -top-[8.75rem] ml-7 leading-none">
+        <span className="text-white flex md:text-[24px] text-[18px] text-opacity-50 uppercase">
           {"Creative agency"}
         </span>
-        <span className="text-white text-6xl text-[88px]">
+        <span className="text-white md:text-[5.5rem] text-[40px] leading-none">
           {"Whitedog Media"}
         </span>
       </div>
