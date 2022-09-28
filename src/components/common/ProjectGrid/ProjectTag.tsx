37bc74a8ab95f1a02815c1643f1bc3c6f3559593
Armand Gonda <armand.gonda@5martstudio.com>
2022-09-28 20:09:55 +0200

    feat: add hover animation to project tags

--- src/components/common/ProjectGrid/ProjectTag.tsx
+++ src/components/common/ProjectGrid/ProjectTag.tsx
@@ -6,10 +6,8 @@ interface ProjectTagTypes {
 
 const ProjectTagComponent: FC<ProjectTagTypes> = ({ text }) => {
   return (
-    <div className="py-2 px-4 bg-black bg-opacity-[48%] rounded-full mr-1">
-      <span className="text-white text-opacity-[72%] capitalize z-10">
-        {text}
-      </span>
+    <div className="py-2 px-4 bg-black bg-opacity-[48%] hover:bg-[#1D1D1D] duration-300 transition-all text-white hover:text-[#C1C1C1] hover:bg-opacity-100 rounded-full mr-1 cursor-pointer">
+      <span className="capitalize z-10">{text}</span>
     </div>
   );
 };
