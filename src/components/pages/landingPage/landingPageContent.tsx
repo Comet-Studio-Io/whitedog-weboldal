9d9a8fe508d36cccfa1875df6b191e9b0f0a22ba
Armand Gonda <armand.gonda@5martstudio.com>
2022-09-28 16:54:17 +0200

    fix: responsive design, replace bad grid asset

--- src/components/pages/landingPage/landingPageContent.tsx
+++ src/components/pages/landingPage/landingPageContent.tsx
@@ -1,7 +1,8 @@
+import clsx from "clsx";
 import { useRouter } from "next/router";
 import { memo, FC, useCallback } from "react";
-import { isMobile } from "react-device-detect";
 
+import { useGetUserAgent } from "../../../hooks/useGetUserAgent";
 import { Button } from "../../common/Button/Button";
 import { ProjectGrid } from "../../common/ProjectGrid/ProjectGrid";
 import { ProjectGridItem } from "../../common/ProjectGrid/ProjectGridItem";
@@ -13,31 +14,36 @@ const LandingPageContentComponent: FC = () => {
     await router.push("/projects");
   }, []);
 
+  const deviceState = useGetUserAgent();
+
   return (
     <section className="flex flex-col justify-start items-center w-full h-auto md:px-8 px-2 pb-20">
       <div className="flex">
-        <span className="text-white w-2/3 text-[1.75rem] py-6">
+        <span
+          className={clsx("text-white py-6", {
+            "text-[1.375rem]": deviceState === "mobile",
+            "text-[1.75rem] w-2/3": deviceState === "desktop",
+          })}
+        >
           {
             "Az ügyfeleknek valójában nem egyszerűen a brief alapján készülő kreatívokra, hanem üzleti problémájuk megoldására van szükségük — ezért mi kreatív, személyre szabott kommunikációs és üzletviteli megoldásokat nyújtunk számukra."
           }
         </span>
       </div>
 
-      <div className="w-full text-left">
-        <span className="uppercase text-text-gray-light pt-14 pb-8">
-          {"Projektek"}
-        </span>
+      <div className="w-full text-left pt-20 pb-8">
+        <span className="uppercase text-text-gray-light">{"Projektek"}</span>
       </div>
-      {isMobile ? (
+      {deviceState === "mobile" ? (
         <ProjectGrid columns={5} rows={15}>
           <ProjectGridItem
             colSpan={5}
-            imgSrc={"/images/projects/epkar.gif"}
+            imgSrc={"/images/projects/high-tech-suli.gif"}
             rowEnd={4}
             rowStart={1}
             tag1={"branding"}
             tag2={"advertising"}
-            title={"épkar"}
+            title={"high tech suli"}
           />
           <ProjectGridItem
             colSpan={5}
