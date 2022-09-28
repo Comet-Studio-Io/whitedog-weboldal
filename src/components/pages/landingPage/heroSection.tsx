22975153d366076825ebae69662ff8c213b94188
Armand Gonda <armand.gonda@5martstudio.com>
2022-09-28 16:55:12 +0200

    fix(hero section): responsiveness, mobile asset

--- src/components/pages/landingPage/heroSection.tsx
+++ src/components/pages/landingPage/heroSection.tsx
@@ -1,18 +1,32 @@
+import clsx from "clsx";
 import Image from "next/image";
 import { memo, FC } from "react";
 
+import { useGetUserAgent } from "../../../hooks/useGetUserAgent";
+
 const HeroSectionComponent: FC = (): JSX.Element => {
+  const deviceState = useGetUserAgent();
+
   return (
-    <section className="w-full h-auto flex flex-col">
+    <section className={clsx("w-full h-auto flex flex-col min-h-screen")}>
       <Image
         alt="hero-image"
-        className="absolute"
         height={720}
-        layout="responsive"
-        src="/images/hero.png"
+        layout={deviceState === "desktop" ? "responsive" : "fill"}
+        src={
+          deviceState === "desktop"
+            ? "/images/hero.png"
+            : "/images/hero_mobile.png"
+        }
         width={1440}
       />
-      <div className="relative -top-[8.75rem] ml-7 leading-none">
+      <div
+        className={clsx("leading-none relative", {
+          "flex flex-col justify-end mx-2 bottom-4 min-h-[100vh]":
+            deviceState === "mobile",
+          "-top-[8.75rem] ml-7": deviceState === "desktop",
+        })}
+      >
         <span className="text-white flex md:text-[24px] text-[18px] text-opacity-50 uppercase">
           {"Creative agency"}
         </span>
