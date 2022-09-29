01bbb5879171cc1a64f4ecf767aaf1c4a3fc5eec
Armand Gonda <armand.gonda@5martstudio.com>
2022-09-29 18:49:56 +0200

    feat(header links): add hover state

--- src/components/common/Header/HeaderLink.tsx
+++ src/components/common/Header/HeaderLink.tsx
@@ -5,7 +5,10 @@ interface HeaderLinkTypes {
 }
 const HeaderLinkComponent: FC<HeaderLinkTypes> = ({ link }) => {
   return (
-    <a className="uppercase md:px-4 px-2 text-white opacity-50" href={link}>
+    <a
+      className="uppercase md:px-4 px-2 text-white text-opacity-50 hover:text-opacity-100 transition-all duration-300"
+      href={link}
+    >
       {link}
     </a>
   );
