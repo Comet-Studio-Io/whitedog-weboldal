6ecde97463f15029b84329dfd09267215032045f
Armand Gonda <armand.gonda@5martstudio.com>
2022-09-30 14:59:53 +0200

    feat: add responsiveness

--- src/components/common/Footer/FooterCarouselItem.tsx
+++ src/components/common/Footer/FooterCarouselItem.tsx
@@ -1,14 +1,28 @@
+import clsx from "clsx";
 import Image from "next/image";
 import { memo, FC } from "react";
+
+import { useGetUserAgent } from "../../../hooks/useGetUserAgent";
 
 interface FooterCarouselItemTypes {
   src: string;
 }
 
 const FooterCarouselItemComponent: FC<FooterCarouselItemTypes> = ({ src }) => {
+  const userAgent = useGetUserAgent();
+
   return (
-    <div className="relative mx-20">
-      <Image height={110} layout={"intrinsic"} src={src} width={260} />
+    <div
+      className={clsx("relative", {
+        "mx-20": userAgent === "desktop",
+        "mx-5": userAgent === "mobile",
+      })}
+    >
+      <Image
+        height={userAgent === "desktop" ? 110 : 80}
+        src={src}
+        width={userAgent === "desktop" ? 260 : 144}
+      />
     </div>
   );
 };
