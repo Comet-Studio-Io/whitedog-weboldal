a3fe3e60679a6fb8bff562a450c0bc23618cc22f
Armand Gonda <armand.gonda@5martstudio.com>
2022-09-30 11:25:32 +0200

    feat: add services section and components

---
+++ src/components/common/Services/ServicesItem.tsx
@@ -0,0 +1,45 @@
+import { Collapse } from "@chakra-ui/react";
+import clsx from "clsx";
+import { FC, memo, MouseEventHandler } from "react";
+
+interface ServiceItemTypes {
+  title: string;
+  data: string;
+  active: boolean;
+  dark?: boolean;
+  onClick: MouseEventHandler<HTMLButtonElement>;
+}
+
+const ServiceItemComponent: FC<ServiceItemTypes> = ({
+  title,
+  data,
+  active,
+  dark = true,
+  onClick,
+}) => {
+  return (
+    <button
+      className={clsx("md:text-[1.75rem] text-[1.375rem] w-full text-left", {
+        "text-black": active && dark,
+        "text-white": active && !dark,
+        "text-text-gray-light": !active,
+      })}
+      type="button"
+      value={title}
+      onClick={onClick}
+    >
+      {active ? "" : title}
+      <Collapse animateOpacity in={active}>
+        {title + data}
+      </Collapse>
+      <hr
+        className={clsx("h-1 w-full border-t-2 border-solid my-4", {
+          "border-black": active,
+          "border-text-gray-light": !active,
+        })}
+      />
+    </button>
+  );
+};
+
+export const ServiceItem = memo(ServiceItemComponent);
--- src/components/pages/landingPage/servicesSection.tsx
+++ src/components/pages/landingPage/servicesSection.tsx
@@ -1,29 +1,78 @@
-import clsx from "clsx";
-import { memo, FC } from "react";
+import { memo, FC, useState, useCallback, MouseEvent } from "react";
 
-import { useGetUserAgent } from "../../../hooks/useGetUserAgent";
+import { ServiceItem } from "../../common/Services/ServicesItem";
+import { Title } from "../../common/Title/Title";
 
 const ServicesSectionComponent: FC = () => {
-  const deviceState = useGetUserAgent();
+  const [selectedSection, setSelectedSection] = useState("Márkafejlesztés");
+
+  const handleServiceItemClick = useCallback(
+    (e: MouseEvent<HTMLButtonElement>) => {
+      setSelectedSection(e.currentTarget.value);
+    },
+    [],
+  );
 
   return (
-    <section className="flex flex-col justify-start items-center w-full h-auto md:px-8 px-2 pb-20">
-      <div className="flex">
-        <span
-          className={clsx("text-white py-6", {
-            "text-[1.375rem]": deviceState === "mobile",
-            "text-[1.75rem] w-2/3": deviceState === "desktop",
-          })}
-        >
-          {
-            "Az ügyfeleknek valójában nem egyszerűen a brief alapján készülő kreatívokra, hanem üzleti problémájuk megoldására van szükségük — ezért mi kreatív, személyre szabott kommunikációs és üzletviteli megoldásokat nyújtunk számukra."
-          }
-        </span>
-      </div>
-
-      <div className="w-full text-left pt-20 pb-8">
-        <span className="uppercase text-text-gray-light">{"Projektek"}</span>
-      </div>
+    <section className="flex flex-col justify-start items-center w-full h-auto md:px-8 px-2 pb-20 bg-white">
+      <Title className={"text-black"} text={"szolgáltatások"} />
+      <ServiceItem
+        active={selectedSection === "Márkafejlesztés"}
+        data=" — Amikor márkát építünk, célunk, hogy definiáljuk és kifejezzük azt a jelentőségteljes különbséget, ami a vállalat hosszútávú sikerének az alapja."
+        title="Márkafejlesztés"
+        onClick={handleServiceItemClick}
+      />
+      <ServiceItem
+        active={selectedSection === "Arculatfejlesztés"}
+        data=" — Ügyfeleink arculatát a legújabb trendeket és piaci kihívásokat szem előtt tartva fejlesztjük, minden részletre odafigyelve."
+        title="Arculatfejlesztés"
+        onClick={handleServiceItemClick}
+      />
+      <ServiceItem
+        active={selectedSection === "Kampányfejlesztés"}
+        data=" – A komplex integrált kampányoktól kezdve az egycsatornás taktikai kampányokig minden feladat egyformán fontos számunkra"
+        title="Kampányfejlesztés"
+        onClick={handleServiceItemClick}
+      />
+      <ServiceItem
+        active={selectedSection === "Filmgyártás"}
+        data=" – Ma a marketingkommunikáció egyik leghatékonyabb eszköze a film, és a videó. Mi kihozzuk minden témából a maximumot: hisszük, hogy tartalom és a minőség egymást erősítve egyaránt fontos valamennyi produkciónál."
+        title="Filmgyártás"
+        onClick={handleServiceItemClick}
+      />
+      <ServiceItem
+        active={selectedSection === "Webfejlesztés"}
+        data=" – A teljeskörű website-fejlesztés a megfelelő stratégia kidolgozásával kezdődik, akár egy egyszerű microsite-ról, akár egy corporate weboldalról van szó."
+        title="Webfejlesztés"
+        onClick={handleServiceItemClick}
+      />
+      <ServiceItem
+        active={selectedSection === "Social media tanácsadás és management"}
+        data=" – A marketing- és a kommunikációs stratégiával összehangolt social media megoldásokat készítünk."
+        title="Social media tanácsadás és management"
+        onClick={handleServiceItemClick}
+      />
+      <ServiceItem
+        active={selectedSection === "PR tanácsadás és management"}
+        data=" – A marketing- és kommunikációs startégia mentén széles körű PR-megoldásokat dolgozunk ki."
+        title="PR tanácsadás és management"
+        onClick={handleServiceItemClick}
+      />
+      <ServiceItem
+        active={selectedSection === "Médiatervezés és médiavásárlás"}
+        data=" – A médiastratégiát, a –tervezést, illetve –vásárlást az üzleti- és marketingcélokra építve dolgozzuk ki."
+        title="Médiatervezés és médiavásárlás"
+        onClick={handleServiceItemClick}
+      />
+      <ServiceItem
+        active={
+          selectedSection ===
+          "Általános marketing tanácsadás, vállalatfejlesztés és üzletviteli tanácsadás"
+        }
+        data=" – Általános marketingtanácsadás, illetve vállalatfejlesztés és üzletviteli tanácsadás keretében a partnerünk működésének optimalizálására és növekedési lehetőségeire készítünk javaslatot."
+        title="Általános marketing tanácsadás, vállalatfejlesztés és üzletviteli tanácsadás"
+        onClick={handleServiceItemClick}
+      />
     </section>
   );
 };
