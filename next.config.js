2aa9967838cc3e0100a339f1b83b49c38fff897e
Armand Gonda <armand.gonda@5martstudio.com>
2022-09-30 18:11:31 +0200

    chore: add svgr plugin to next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withSvgr = require("next-plugin-svgr");
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = withSvgr(nextConfig);
