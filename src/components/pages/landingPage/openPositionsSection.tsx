d5558423a3f0ae416744d1ca22fd3de2e0e4093b
Armand Gonda <armand.gonda@5martstudio.com>
2022-10-03 11:58:28 +0200

    refactor: open positions and services section improvements

--- src/components/pages/landingPage/openPositionsSection.tsx
+++ src/components/pages/landingPage/openPositionsSection.tsx
@@ -1,6 +1,6 @@
-import { memo, FC, useState, useCallback, MouseEvent } from "react";
+import { memo, FC, useState, useCallback, MouseEvent, useMemo } from "react";
 
-import { ServiceItem } from "../../common/Services/ServicesItem";
+import { ListItem } from "../../common/List/ListItem";
 import { Title } from "../../common/Title/Title";
 
 const OpenPositionsSectionComponent: FC = () => {
@@ -13,30 +13,39 @@ const OpenPositionsSectionComponent: FC = () => {
     [],
   );
 
+  const openPositionsList = useMemo(
+    () => [
+      {
+        data: " — Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec interdum neque, sit amet dapibus nibh. Mauris sed neque eu nulla ultrices bibendum id nec massa. Quisque laoreet vehicula urna, ut pretium diam pelda.email@whitedogmedia.hu vitae.",
+        title: "Account Manager",
+      },
+      {
+        data: " — Ügyfeleink arculatát a legújabb trendeket és piaci kihívásokat szem előtt tartva fejlesztjük, minden részletre odafigyelve.",
+        title: "Production Manager",
+      },
+      {
+        data: " — A komplex integrált kampányoktól kezdve az egycsatornás taktikai kampányokig minden feladat egyformán fontos számunkra",
+        title: "Junior Account Manager",
+      },
+    ],
+    [],
+  );
+
   return (
     <section className="flex flex-col justify-start items-center w-full h-auto md:px-8 px-2 pb-20 bg-black">
       <Title className={"text-white"} text={"nyitott pozíciók"} />
-      <ServiceItem
-        active={selectedSection === "Account Manager"}
-        dark={false}
-        data="— Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec interdum neque, sit amet dapibus nibh. Mauris sed neque eu nulla ultrices bibendum id nec massa. Quisque laoreet vehicula urna, ut pretium diam pelda.email@whitedogmedia.hu vitae."
-        title="Account Manager"
-        onClick={handleServiceItemClick}
-      />
-      <ServiceItem
-        active={selectedSection === "Production Manager"}
-        dark={false}
-        data=" — Ügyfeleink arculatát a legújabb trendeket és piaci kihívásokat szem előtt tartva fejlesztjük, minden részletre odafigyelve."
-        title="Production Manager"
-        onClick={handleServiceItemClick}
-      />
-      <ServiceItem
-        active={selectedSection === "Junior Account Manager"}
-        dark={false}
-        data=" – A komplex integrált kampányoktól kezdve az egycsatornás taktikai kampányokig minden feladat egyformán fontos számunkra"
-        title="Junior Account Manager"
-        onClick={handleServiceItemClick}
-      />
+      {openPositionsList.map(service => {
+        return (
+          <ListItem
+            key={service.title}
+            active={selectedSection === service.title}
+            dark={false}
+            data={service.data}
+            title={service.title}
+            onClick={handleServiceItemClick}
+          />
+        );
+      })}
     </section>
   );
 };
--- src/components/pages/landingPage/servicesSection.tsx
+++ src/components/pages/landingPage/servicesSection.tsx
@@ -1,10 +1,53 @@
-import { memo, FC, useState, useCallback, MouseEvent } from "react";
+import { memo, FC, useState, useCallback, MouseEvent, useMemo } from "react";
 
-import { ServiceItem } from "../../common/Services/ServicesItem";
+import { ListItem } from "../../common/List/ListItem";
 import { Title } from "../../common/Title/Title";
 
 const ServicesSectionComponent: FC = () => {
-  const [selectedSection, setSelectedSection] = useState("Márkafejlesztés");
+  const servicesList = useMemo(
+    () => [
+      {
+        title: "Márkafejlesztés",
+        data: " — Amikor márkát építünk, célunk, hogy definiáljuk és kifejezzük azt a jelentőségteljes különbséget, ami a vállalat hosszútávú sikerének az alapja.",
+      },
+      {
+        title: "Arculatfejlesztés",
+        data: " — Ügyfeleink arculatát a legújabb trendeket és piaci kihívásokat szem előtt tartva fejlesztjük, minden részletre odafigyelve.",
+      },
+      {
+        title: "Kampányfejlesztés",
+        data: " — A komplex integrált kampányoktól kezdve az egycsatornás taktikai kampányokig minden feladat egyformán fontos számunkra",
+      },
+      {
+        title: "Filmgyártás",
+        data: " — Ma a marketingkommunikáció egyik leghatékonyabb eszköze a film, és a videó. Mi kihozzuk minden témából a maximumot: hisszük, hogy tartalom és a minőség egymást erősítve egyaránt fontos valamennyi produkciónál.",
+      },
+      {
+        title: "Webfejlesztés",
+        data: " — A teljeskörű website-fejlesztés a megfelelő stratégia kidolgozásával kezdődik, akár egy egyszerű microsite-ról, akár egy corporate weboldalról van szó.",
+      },
+      {
+        title: "Social media tanácsadás és management",
+        data: " — A marketing- és a kommunikációs stratégiával összehangolt social media megoldásokat készítünk.",
+      },
+      {
+        title: "PR tanácsadás és management",
+        data: " — A marketing- és kommunikációs startégia mentén széles körű PR-megoldásokat dolgozunk ki.",
+      },
+      {
+        title: "Médiatervezés és médiavásárlás",
+        data: " — A médiastratégiát, a –tervezést, illetve –vásárlást az üzleti- és marketingcélokra építve dolgozzuk ki.",
+      },
+      {
+        title:
+          "Általános marketing tanácsadás, vállalatfejlesztés és üzletviteli tanácsadás",
+        data: " — Általános marketingtanácsadás, illetve vállalatfejlesztés és üzletviteli tanácsadás keretében a partnerünk működésének optimalizálására és növekedési lehetőségeire készítünk javaslatot.",
+      },
+    ],
+    [],
+  );
+
+  const [selectedSection, setSelectedSection] = useState(servicesList[0].title);
 
   const handleServiceItemClick = useCallback(
     (e: MouseEvent<HTMLButtonElement>) => {
@@ -16,63 +59,17 @@ const ServicesSectionComponent: FC = () => {
   return (
     <section className="flex flex-col justify-start items-center w-full h-auto md:px-8 px-2 pb-20 bg-white">
       <Title className={"text-black"} text={"szolgáltatások"} />
-      <ServiceItem
-        active={selectedSection === "Márkafejlesztés"}
-        data=" — Amikor márkát építünk, célunk, hogy definiáljuk és kifejezzük azt a jelentőségteljes különbséget, ami a vállalat hosszútávú sikerének az alapja."
-        title="Márkafejlesztés"
-        onClick={handleServiceItemClick}
-      />
-      <ServiceItem
-        active={selectedSection === "Arculatfejlesztés"}
-        data=" — Ügyfeleink arculatát a legújabb trendeket és piaci kihívásokat szem előtt tartva fejlesztjük, minden részletre odafigyelve."
-        title="Arculatfejlesztés"
-        onClick={handleServiceItemClick}
-      />
-      <ServiceItem
-        active={selectedSection === "Kampányfejlesztés"}
-        data=" – A komplex integrált kampányoktól kezdve az egycsatornás taktikai kampányokig minden feladat egyformán fontos számunkra"
-        title="Kampányfejlesztés"
-        onClick={handleServiceItemClick}
-      />
-      <ServiceItem
-        active={selectedSection === "Filmgyártás"}
-        data=" – Ma a marketingkommunikáció egyik leghatékonyabb eszköze a film, és a videó. Mi kihozzuk minden témából a maximumot: hisszük, hogy tartalom és a minőség egymást erősítve egyaránt fontos valamennyi produkciónál."
-        title="Filmgyártás"
-        onClick={handleServiceItemClick}
-      />
-      <ServiceItem
-        active={selectedSection === "Webfejlesztés"}
-        data=" – A teljeskörű website-fejlesztés a megfelelő stratégia kidolgozásával kezdődik, akár egy egyszerű microsite-ról, akár egy corporate weboldalról van szó."
-        title="Webfejlesztés"
-        onClick={handleServiceItemClick}
-      />
-      <ServiceItem
-        active={selectedSection === "Social media tanácsadás és management"}
-        data=" – A marketing- és a kommunikációs stratégiával összehangolt social media megoldásokat készítünk."
-        title="Social media tanácsadás és management"
-        onClick={handleServiceItemClick}
-      />
-      <ServiceItem
-        active={selectedSection === "PR tanácsadás és management"}
-        data=" – A marketing- és kommunikációs startégia mentén széles körű PR-megoldásokat dolgozunk ki."
-        title="PR tanácsadás és management"
-        onClick={handleServiceItemClick}
-      />
-      <ServiceItem
-        active={selectedSection === "Médiatervezés és médiavásárlás"}
-        data=" – A médiastratégiát, a –tervezést, illetve –vásárlást az üzleti- és marketingcélokra építve dolgozzuk ki."
-        title="Médiatervezés és médiavásárlás"
-        onClick={handleServiceItemClick}
-      />
-      <ServiceItem
-        active={
-          selectedSection ===
-          "Általános marketing tanácsadás, vállalatfejlesztés és üzletviteli tanácsadás"
-        }
-        data=" – Általános marketingtanácsadás, illetve vállalatfejlesztés és üzletviteli tanácsadás keretében a partnerünk működésének optimalizálására és növekedési lehetőségeire készítünk javaslatot."
-        title="Általános marketing tanácsadás, vállalatfejlesztés és üzletviteli tanácsadás"
-        onClick={handleServiceItemClick}
-      />
+      {servicesList.map(service => {
+        return (
+          <ListItem
+            key={service.title}
+            active={selectedSection === service.title}
+            data={service.data}
+            title={service.title}
+            onClick={handleServiceItemClick}
+          />
+        );
+      })}
     </section>
   );
 };
