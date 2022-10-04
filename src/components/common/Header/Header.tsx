1d1bd664fe35256a65b50889ad187d913e55024a
Armand Gonda <armand.gonda@5martstudio.com>
2022-10-04 20:09:27 +0200

    feat: add homepage link to logo

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -11,12 +11,15 @@ const HeaderComponent: FC = (): JSX.Element => {
 
   return (
     <header className="h-12 justify-between w-full md:px-8 px-2 items-center flex flex-row z-20 relative">
-      <Image
-        alt="header-icon"
-        height={24}
-        src="/images/headerIcon.svg"
-        width={43}
-      />
+      <a href={webPaths.home}>
+        <Image
+          priority
+          alt="header-icon"
+          height={24}
+          src="/images/headerIcon.svg"
+          width={43}
+        />
+      </a>
       <div className="space-x-6">
         <HeaderLink href={webPaths.projects} text={"Projektek"} />
         {userAgent === "desktop" && (
