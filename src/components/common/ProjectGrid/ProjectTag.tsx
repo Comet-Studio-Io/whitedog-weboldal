9b40b0600a7bb3f1ba300da5008be7a24460750b
Armand Gonda <armand.gonda@5martstudio.com>
2022-10-04 17:54:56 +0200

    refactor(projectTag): refactor to make it reusable for standalone project pages

--- src/components/common/ProjectGrid/ProjectTag.tsx
+++ src/components/common/ProjectGrid/ProjectTag.tsx
@@ -1,12 +1,25 @@
+import clsx from "clsx";
 import { FC, memo } from "react";
 
 interface ProjectTagTypes {
   text: string;
+  darkMode?: boolean;
 }
 
-const ProjectTagComponent: FC<ProjectTagTypes> = ({ text }) => {
+const ProjectTagComponent: FC<ProjectTagTypes> = ({
+  text,
+  darkMode = false,
+}) => {
   return (
-    <div className="py-2 px-4 bg-black bg-opacity-[48%] hover:bg-[#1D1D1D] duration-300 transition-all text-white hover:text-[#C1C1C1] hover:bg-opacity-100 rounded-full mr-1 cursor-pointer">
+    <div
+      className={clsx(
+        "py-2 px-4  duration-300 transition-all text-white hover:text-[#C1C1C1] hover:bg-opacity-100 rounded-full mr-1 cursor-pointer",
+        {
+          "hover:bg-opacity-[48%] bg-[#1D1D1D]": darkMode,
+          "bg-black bg-opacity-[48%] hover:bg-[#1D1D1D]": !darkMode,
+        },
+      )}
+    >
       <span className="capitalize z-10">{text}</span>
     </div>
   );
