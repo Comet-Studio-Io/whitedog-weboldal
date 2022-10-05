9e9649aaec7c1e6afe6dde79951faeeeafea8137
Armand Gonda <armand.gonda@5martstudio.com>
2022-10-05 12:25:30 +0200

    fix: unused-var build fail

--- src/components/pages/specificProjectPage/[project].tsx
+++ src/components/pages/specificProjectPage/[project].tsx
@@ -1,8 +1,6 @@
 import { memo, FC } from "react";
 
 import { SpecificProjectPageTypes } from "../../../types/specificProjectPageTypes";
-import { ProjectGrid } from "../../common/ProjectGrid/ProjectGrid";
-import { ProjectGridItem } from "../../common/ProjectGrid/ProjectGridItem";
 import { ProjectTags } from "../../common/ProjectGrid/ProjectTags";
 import { PageTitle } from "../../common/Title/PageTitle";
 import { SubTitle } from "../../common/Title/SubTitle";
@@ -19,62 +17,6 @@ const SpecificProjectPageComponent: FC<SpecificProjectPageTypes> = ({
       </div>
       <PageTitle className="text-white pt-4" text={projectData.title} />
       <SubTitle className="text-white pt-4 pb-8" text={projectData.company} />
-      {/* <ProjectGrid columns={7} rows={21}>
-        <ProjectGridItem
-          colEnd={5}
-          colStart={1}
-          company="Cég"
-          imgSrc={"/images/projects/epkar.gif"}
-          projectTitle="Cég termék kampány"
-          rowEnd={6}
-          rowStart={1}
-          tagArray={["branding", "advertising"]}
-          title={"épkar"}
-        />
-        <ProjectGridItem
-          colEnd={8}
-          colStart={5}
-          company="Cég"
-          imgSrc={"/images/projects/gallio.gif"}
-          projectTitle="Cég termék kampány"
-          rowEnd={5}
-          rowStart={1}
-          tagArray={["branding"]}
-          title={"gallio"}
-        />
-        <ProjectGridItem
-          colSpan={7}
-          company="Cég"
-          imgSrc={"/images/projects/ds.gif"}
-          projectTitle="Cég termék kampány"
-          rowEnd={15}
-          rowStart={6}
-          tagArray={["branding"]}
-          title={"dustin"}
-        />
-        <ProjectGridItem
-          colEnd={4}
-          colStart={1}
-          company="Cég"
-          imgSrc={"/images/projects/mkb-lakasfelujitas.gif"}
-          projectTitle="Cég termék kampány"
-          rowEnd={22}
-          rowStart={17}
-          tagArray={["branding"]}
-          title={"mkb"}
-        />
-        <ProjectGridItem
-          colEnd={8}
-          colStart={4}
-          company="Cég"
-          imgSrc={"/images/projects/ikea-demo.gif"}
-          projectTitle="Cég termék kampány"
-          rowEnd={22}
-          rowStart={15}
-          tagArray={["branding", "advertising"]}
-          title={"épkar"}
-        />
-      </ProjectGrid> */}
     </section>
   );
 };
