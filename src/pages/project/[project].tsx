995d6106150336dce6df8e3928480eb2efbf313f
Armand Gonda <armand.gonda@5martstudio.com>
2022-10-05 18:12:51 +0200

    refactor: remove page props from url

--- src/pages/project/[project].tsx
+++ src/pages/project/[project].tsx
@@ -1,36 +1,10 @@
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-
 import { DefaultLayout } from "../../components/layouts/defaultLayout";
 import { SpecificProjectPage } from "../../components/pages/specificProjectPage/[project]";
-import { SpecificProjectPageTypes } from "../../types/specificProjectPageTypes";
 
 const ProjectPage = (): JSX.Element => {
-  const router = useRouter();
-  const [queryState, setQueryState] = useState<SpecificProjectPageTypes>({
-    projectData: {
-      company: "",
-      tags: [],
-      title: "",
-    },
-  });
-
-  useEffect(() => {
-    if (router.query !== undefined) {
-      setQueryState({
-        projectData: {
-          ...queryState,
-          company: router.query.company as string,
-          tags: router.query.tags as string[],
-          title: router.query.title as string,
-        },
-      });
-    }
-  }, [router.query]);
-
   return (
     <DefaultLayout>
-      <SpecificProjectPage projectData={queryState.projectData} />
+      <SpecificProjectPage />
     </DefaultLayout>
   );
 };
