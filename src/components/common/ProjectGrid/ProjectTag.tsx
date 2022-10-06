10efe49605ef08fe36f7ff4d534b145c5ed87295
Armand Gonda <armand.gonda@5martstudio.com>
2022-10-06 21:05:46 +0200

    feat: add project tag filtering

--- src/components/common/ProjectGrid/ProjectTag.tsx
+++ src/components/common/ProjectGrid/ProjectTag.tsx
@@ -1,5 +1,9 @@
 import clsx from "clsx";
-import { FC, memo } from "react";
+import { useRouter } from "next/router";
+import { FC, memo, MouseEvent, useCallback } from "react";
+
+import { useZustandStore } from "../../../store/useStore";
+import { webPaths } from "../../../webpaths";
 
 interface ProjectTagTypes {
   text: string;
@@ -10,18 +14,31 @@ const ProjectTagComponent: FC<ProjectTagTypes> = ({
   text,
   darkMode = false,
 }) => {
+  const router = useRouter();
+  const handleTagClick = useCallback(
+    (e: MouseEvent<HTMLButtonElement>) => {
+      e.stopPropagation();
+      useZustandStore.setState({ selectedTag: [text] });
+      void router.push(webPaths.projects);
+    },
+    [text],
+  );
+
   return (
-    <div
+    <button
       className={clsx(
-        "py-2 px-4  duration-300 transition-all text-white hover:text-[#C1C1C1] hover:bg-opacity-100 rounded-full mr-1 cursor-pointer",
+        "py-2 px-4 z-10 relative duration-300 transition-all text-white hover:text-[#C1C1C1] hover:bg-opacity-100 rounded-full mr-1 cursor-pointer",
         {
           "hover:bg-opacity-[48%] bg-[#1D1D1D]": darkMode,
           "bg-black bg-opacity-[48%] hover:bg-[#1D1D1D]": !darkMode,
         },
       )}
+      type="button"
+      value={text}
+      onClick={handleTagClick}
     >
       <span className="capitalize z-10">{text}</span>
-    </div>
+    </button>
   );
 };
 
