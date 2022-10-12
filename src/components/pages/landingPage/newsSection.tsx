3f07bd25ba2a67ad34a5cd25a2a5f4a7ab64b36b
Armand Gonda <armand.gonda@5martstudio.com>
2022-10-12 19:44:42 +0200

    refactor(news): add data from strapi

--- src/components/pages/landingPage/newsSection.tsx
+++ src/components/pages/landingPage/newsSection.tsx
@@ -2,6 +2,7 @@ import clsx from "clsx";
 import { memo, FC, useMemo, useRef } from "react";
 import { useDraggable } from "react-use-draggable-scroll";
 
+import { useGetNews } from "../../../hooks/useGetNews";
 import { useGetUserAgent } from "../../../hooks/useGetUserAgent";
 import { NewsNavbarTypes } from "../../../types/newsNavbarTypes";
 import { Navbar } from "../../common/Navbar/Navbar";
@@ -11,87 +12,11 @@ import { Title } from "../../common/Title/Title";
 const NewsSectionComponent: FC = () => {
   const userAgent = useGetUserAgent();
 
+  const { data } = useGetNews();
+
   const navbarOptions: NewsNavbarTypes[] = useMemo(() => {
     return ["Összes", "Díjak", "Publikációk", "Sajtómegjelenések"];
   }, []);
-
-  const newsItemsList = useMemo(
-    () => [
-      {
-        date: "Február 12.",
-        src: "/images/news/news1.png",
-        text: "A Whitedog Mediát díjazták a Cannes Corporate Media & TV Awardson",
-        type: "Összes",
-      },
-      {
-        date: "Február 12.",
-        src: "/images/news/news2.png",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
-        type: "Összes",
-      },
-      {
-        date: "Február 12.",
-        src: "/images/news/news3.png",
-        text: "Aenean nec interdum neque, sit amet dapibus nibh. Mauris sed neque eu nulla ultrices bibendum id nec massa. ",
-        type: "Díjak",
-      },
-      {
-        date: "Február 12.",
-        src: "/images/news/news4.png",
-        text: "Duis sed enim nec ligula mattis fringilla vitae quis tortor.",
-        type: "Publikációk",
-      },
-      {
-        date: "Február 12.",
-        src: "/images/news/news1.png",
-        text: "A Whitedog Mediát díjazták a Cannes Corporate Media & TV Awardson",
-        type: "Díjak",
-      },
-      {
-        date: "Február 12.",
-        src: "/images/news/news2.png",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
-        type: "Publikációk",
-      },
-      {
-        date: "Február 12.",
-        src: "/images/news/news3.png",
-        text: "Aenean nec interdum neque, sit amet dapibus nibh. Mauris sed neque eu nulla ultrices bibendum id nec massa. ",
-        type: "Díjak",
-      },
-      {
-        date: "Február 12.",
-        src: "/images/news/news4.png",
-        text: "Duis sed enim nec ligula mattis fringilla vitae quis tortor.",
-        type: "Publikációk",
-      },
-      {
-        date: "Február 12.",
-        src: "/images/news/news1.png",
-        text: "A Whitedog Mediát díjazták a Cannes Corporate Media & TV Awardson",
-        type: "Díjak",
-      },
-      {
-        date: "Február 12.",
-        src: "/images/news/news2.png",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
-        type: "Sajtómegjelenések",
-      },
-      {
-        date: "Február 12.",
-        src: "/images/news/news3.png",
-        text: "Aenean nec interdum neque, sit amet dapibus nibh. Mauris sed neque eu nulla ultrices bibendum id nec massa. ",
-        type: "Sajtómegjelenések",
-      },
-      {
-        date: "Február 12.",
-        src: "/images/news/news4.png",
-        text: "Duis sed enim nec ligula mattis fringilla vitae quis tortor.",
-        type: "Sajtómegjelenések",
-      },
-    ],
-    [],
-  );
 
   const containerRef = useRef<HTMLDivElement>(
     null,
@@ -115,14 +40,17 @@ const NewsSectionComponent: FC = () => {
         })}
       >
         <div className="space-x-3 inline-flex">
-          {newsItemsList.map(service => {
+          {data?.map(newsItem => {
             return (
               <SlideshowItem
-                key={service.text}
-                date={service.date}
-                filterType={service.type}
-                src={service.src}
-                text={service.text}
+                key={newsItem.id}
+                date={newsItem.attributes.data.date}
+                filterType={newsItem.attributes.data.filter}
+                src={
+                  String(process.env.NEXT_PUBLIC_API_URL) +
+                  newsItem.attributes.image.data.attributes.url
+                }
+                text={newsItem.attributes.data.title}
               />
             );
           })}
