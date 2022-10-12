6a963a1e1fb29a87d079e0f3373a86eb78f04153
Armand Gonda <armand.gonda@5martstudio.com>
2022-10-12 11:10:03 +0200

    feat: add strapi

---
+++ backend/src/admin/app.example.js
@@ -0,0 +1,14 @@
+
+export default {
+  config: {
+    locales: ['hu'],
+    translations: {
+      hu: {
+        // Translate a plugin's key/value pair by adding the plugin's name as a prefix
+        // In this case, we translate the "plugin.name" key of plugin "content-type-builder"
+        "content-type-builder.projects": "Constructeur de Type-Contenu",
+      },
+    },
+  },
+  bootstrap() {},
+};
