a88641a20f09eeb760684f264f91375b2ece0060
Armand Gonda <armand.gonda@5martstudio.com>
2022-10-14 12:55:55 +0200

    refactor: updated api

--- src/types/getProjectTypes.ts
+++ src/types/getProjectTypes.ts
@@ -1,33 +1,32 @@
 import { StrapiImage } from "./strapiImage";
+import { StrapiListItem } from "./strapiListItem";
 import { StrapiProjectGridItem } from "./strapiProjectGridItem";
 
 export interface GetProjectTypes {
   attributes: {
     createdAt: string;
     data: {
-      tags: string[];
-      title: string;
-      company: string;
-    };
-    list: Array<{
-      id: number;
-      __component: string;
-      data: {
+      details: {
+        tags: string[];
         title: string;
-        data: string;
+        company: string;
       };
-    }>;
-    relatedProjects: Array<{
-      id: number;
-      data: StrapiProjectGridItem;
-      image: StrapiImage;
-    }>;
-    ProjectGridItem: Array<{
-      id: number;
-      data: StrapiProjectGridItem;
-      image: StrapiImage;
-    }>;
-    image: StrapiImage;
+      ListItem: Array<{
+        id: number;
+        data: StrapiListItem;
+      }>;
+      RelatedProjects: Array<{
+        id: number;
+        data: StrapiProjectGridItem;
+        image: StrapiImage;
+      }>;
+      ProjectGridItem: Array<{
+        id: number;
+        data: StrapiProjectGridItem;
+        image: StrapiImage;
+      }>;
+    };
+
     locale: string;
     publishedAt: string;
     updatedAt: string;
