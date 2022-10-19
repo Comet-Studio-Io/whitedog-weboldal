5801e8057531ceeacddfda89ac231a163f54c7a5
Armand Gonda <armand.gonda@5martstudio.com>
2022-10-19 12:24:31 +0200

    fix: add auth token to env

--- frontend/src/utils/ajax.ts
+++ frontend/src/utils/ajax.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const axiosHeaders = {
-  Authorization:
-    "bearer 9fdca53e142a3ca30a947425fb7a47ebbd4ef94116ee205951a5c272ec50b28b2b853f0f069f39e571b9d58c9a7fbad0d90cde84c9c440193fe1a22a435a4cf3806f646f6bf0aa5b4b68ab4878c96e4e7ef73b2e572da32b1543d4f8240f390fabe20195693a8fafda24bca6718f1213a4d8195595f73953d03fa660f656f6b3",
+  Authorization: process.env.NEXT_PUBLIC_API_TOKEN,
 };
 
 const apiBaseUrl = `${
