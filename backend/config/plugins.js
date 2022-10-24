0264a56cb54090a8e90c9da025ddf4e8f4b6c24b
Armand Gonda <armand.gonda@5martstudio.com>
2022-10-24 19:36:23 +0200

    build: add slugify plugin

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,4 +1,15 @@
 module.exports = ({ env }) => ({
+  slugify: {
+    enabled: true,
+    config: {
+      contentTypes: {
+        article: {
+          field: 'slug',
+          references: 'title',
+        },
+      },
+    },
+  },
   upload: {
     config: {
       provider: "cloudinary",
