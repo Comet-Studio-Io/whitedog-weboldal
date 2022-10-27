c07b5bf7b22f7ff31c4f64b48f3211affb120ce6
Armand Gonda <armand.gonda@5martstudio.com>
2022-10-27 11:21:37 +0200

    refactor(project filter): update filtering algorithm for tag changes

--- frontend/src/components/common/ProjectGrid/ProjectGrid.tsx
+++ frontend/src/components/common/ProjectGrid/ProjectGrid.tsx
@@ -35,7 +35,11 @@ const ProjectGridComponent: FC<ProjectGridTypes> = ({
         : projectFilter === "Összes" && selectedTag !== undefined
         ? children.filter((e: ReactElement) =>
             selectedTag.every((tag: string) => {
-              return e.props?.tagArray?.includes(tag);
+              return e.props.tagArray?.some(
+                (childTag: { attributes: { name: string } }) => {
+                  return childTag.attributes.name === tag;
+                },
+              );
             }),
           )
         : projectFilter !== "Összes" && selectedTag !== undefined
@@ -43,7 +47,11 @@ const ProjectGridComponent: FC<ProjectGridTypes> = ({
             selectedTag.every((tag: string) => {
               return (
                 projectFilter.includes(e.props?.filterType) &&
-                e.props?.tagArray?.includes(tag)
+                e.props.tagArray?.some(
+                  (childTag: { attributes: { name: string } }) => {
+                    return childTag.attributes.name === tag;
+                  },
+                )
               );
             }),
           )
