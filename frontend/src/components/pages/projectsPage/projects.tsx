a34d7c4bb9a77fc2aab6e533de48ecc85a9f0054
henczpeter <hencz.peter@5martstudio.com>
2022-11-23 14:10:56 +0100

    filter test

--- frontend/src/components/pages/projectsPage/projects.tsx
+++ frontend/src/components/pages/projectsPage/projects.tsx
@@ -10,8 +10,7 @@ import { Title } from "../../common/Title/Title";
 
 const ProjectsPageComponent: FC = (): JSX.Element => {
   const navbarOptions: ProjectFilterTypes[] = useMemo(() => {
-    return ["szia szerelmem, hianyozni fogsz"];
-    return [];
+    return ["Összes", "Arculat", "Filmgyártás", "Webfejlesztés"];
   }, []);
 
   const deviceState = useGetUserAgent();
